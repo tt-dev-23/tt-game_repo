2cf507d5842739dec4f73c83d2457e612f09cf25
Voldemar <63954046+V0l9emar@users.noreply.github.com>
2023-04-20 16:26:18 +0400

    addAlexCorrectedPart

--- index.js
+++ index.js
@@ -1,48 +1,47 @@
-const button = document.getElementById("main__button");
-const pictureField = document.getElementById("main__start-game");
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let pictures;
-const shufle = [];
-const myRes = [];
+const AlexFunction = () => {
+  const randomElements = (array) => {
+    if (!array) {
+      return;
+    }
+    return [...array].sort(() => Math.random() - 0.5);
+  };
+  const button = document.getElementById("main__button");
+  const pictureField = document.getElementById("main__start-game");
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const shuffleNumbers = randomElements(numbers);
+  let pictures;
+  const myRes = [];
 
-
-const myClick = (element) => {
-  element.innerText = "Game Started";
   button.disabled = true;
-  randomElements();
+
+  const answerCheck = (index) => {
+    pictures.addEventListener("click", (event) => {
+        if (numbers[myRes.length] === shuffleNumbers[index]) {
+          event.target.style.opacity = "0.33";
+          myRes.push(shuffleNumbers[index]);
+          if (myRes.length === shuffleNumbers.length) {
+            alert("Stop!");
+          }
+        } else {
+          alert("Wrong");
+        }
+    });
+  };
+
+  const createElements = () => {
+    for (let i = 0; i < shuffleNumbers.length; i++) {
+      pictures = document.createElement("div");
+      pictures.id = shuffleNumbers[i];
+      pictures.classList.add("new-picture");
+      pictures.textContent = shuffleNumbers[i];
+      pictureField.appendChild(pictures);
+      answerCheck(i);
+    }
+  };
   createElements();
 };
-
-const randomElements = () => {
-  shufle.push(numbers.sort(() => Math.random() - 0.5));
-  console.log(shufle);
+const myClick = (element) => {
+  element.innerText = "Game Started";
+  // VoldemarFunction()
+  AlexFunction();
 };
-
-const createElements = () => {
-  for (let i = 0; i < numbers.length; i++) {
-    pictures = document.createElement("div");
-    pictures.id = numbers[i];
-    pictures.classList.add("new-picture");
-    pictures.textContent = numbers[i];
-    pictureField.appendChild(pictures);
-    answerCheck();
-  }
-};
-
-const answerCheck = () => {
-  pictures.addEventListener("click", (event) => {
-    let targetId = event.target.id;
-    if (targetId == pictures.id) {
-      event.target.style.backgroundColor = "salmon";
-      myRes.push(targetId);
-    } else {
-      event.target.style.backgroundColor = "silver";
-    }
-
-    console.log(targetId);
-    console.log(pictures.id);
-  });
-};
-
-console.log(myRes);
-console.log(shufle);
