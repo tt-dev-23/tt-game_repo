55c3ba613f2d8500102126f5bb621c71d3925455
Voldemar <63954046+V0l9emar@users.noreply.github.com>
2023-05-04 00:21:52 +0400

    SolveRecomendation

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-const MainFunction = () => {
-  const randomElements = (array) => {
-    if (!array) {
-      return;
-    }
-    return [...array].sort(() => Math.random() - 0.5);
-  };
-  const button = document.getElementById("main__button");
-  const timeCounter = document.getElementById("main__time-counter");
-  const pictureField = document.getElementById("main__start-game");
-  const modalPage = document.getElementById("modalId");
-  const closeModal = document.getElementsByClassName("modal__class-close")[0];
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const button = document.getElementById("main__button");
+const timeCounter = document.getElementById("main__time-counter");
+const picturesField = document.getElementById("main__start-game");
+const modalPage = document.getElementById("modalId");
+const closeModal = document.getElementsByClassName("modal__class-close")[0];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const myRes = [];
+let picture;
+let timerId;
+let startTime;
+let elTime = 0;
+
+const randomElements = (array) => {
+  if (!array) {
+    return;
+  }
+  return [...array].sort(() => Math.random() - 0.5);
+};
+
+const formatTime = (ms) => {
+  const sec = Math.floor((ms % 60000) / 1000);
+  const msec = ms % 1000;
+  return `${sec.toString().padStart(1, "0")}:${msec
+    .toString()
+    .padStart(3, "0")
+    .slice(0, 2)}`;
+};
+
+const main = () => {
   const shuffleNumbers = randomElements(numbers);
-  const myRes = [];
-  let pictures;
-  let timerId;
-  let startTime;
-  let elTime = 0;
 
   button.disabled = true;
-
-  const formatTime = (ms) => {
-    const sec = Math.floor((ms % 60000) / 1000);
-    const msec = ms % 1000;
-    return `${sec.toString().padStart(1, "0")}:${msec
-      .toString()
-      .padStart(3, "0")
-      .slice(0, 2)}`;
-  };
 
   const startTimer = () => {
     startTime = Date.now() - elTime;
@@ -50,30 +52,27 @@ const MainFunction = () => {
     location.reload();
   };
 
-  window.onclick = (event) => {
+  modalPage.onclick = (event) => {
     if (event.target == modalPage) {
       modalPage.style.display = "none";
       location.reload();
     }
   };
 
-  const buttoEnable = () => {
+  const buttonEnable = () => {
     button.disabled = false;
-    console.log(button.innerText);
     button.innerText = "START";
   };
 
   const answerCheck = (index) => {
-    pictures.addEventListener("click", (event) => {
+    picture.addEventListener("click", (event) => {
       if (numbers[myRes.length] === shuffleNumbers[index]) {
         event.target.style.opacity = "0.33";
         myRes.push(shuffleNumbers[index]);
-        // console.log(myRes.length);
-        // console.log(shuffleNumbers[index]);
         if (myRes.length === shuffleNumbers.length) {
           stopTimer();
           openModal();
-          buttoEnable();
+          buttonEnable();
         }
       } else {
         alert("Wrong");
@@ -83,18 +82,18 @@ const MainFunction = () => {
 
   const createElements = () => {
     for (let i = 0; i < shuffleNumbers.length; i++) {
-      pictures = document.createElement("div");
-      pictures.classList.add("new-picture");
-      pictures.textContent = shuffleNumbers[i];
-      pictureField.appendChild(pictures);
+      picture = document.createElement("div");
+      picture.classList.add("new-picture");
+      picture.textContent = shuffleNumbers[i];
+      picturesField.appendChild(picture);
       answerCheck(i);
     }
   };
   createElements();
   startTimer();
 };
-const myClick = (element) => {
+
+button.onclick = (element) => {
   element.innerText = "Game Started";
-  // VoldemarFunction()
-  MainFunction();
+  main();
 };
